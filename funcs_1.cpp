ae9a29be1f545a83fcab5a3b7f632426723c777e
Stephan Zhdanov <55328925+ret7020@users.noreply.github.com>
2022-09-25 20:28:13 +0300

    Update funcs_1.cpp

--- funcs_1.cpp
+++ funcs_1.cpp
@@ -1,12 +1,21 @@
 #include "easy.h"
 
-double itc_pow(int num, int step){
-    int n = num;
-    for (int i = 0; i < step - 1; i++){
-        n *= num;
-    }
-    return n;
+double itc_pow(int num, int step) {
+    double res = 1;
+    long long tmp_step;
+    if (step < 0)
+        tmp_step = -1 * step;
+    else
+        tmp_step = step;
+
+    for (long long i = 0; i < tmp_step; ++i)
+        res *= num;
+
+    if (step < 0)
+        res = 1 / res;
+    return res;
 }
+
 bool itc_ispositive(int num){
     return num >= 0;
 }
